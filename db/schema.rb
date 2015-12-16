6867fa6bcd7c45ae03d30b966a88f9fcc840cd1e
Reena <reena.enbake@gmail.com>
2015-12-16 18:25:57 +0530

    album commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216095729) do
+ActiveRecord::Schema.define(version: 20151216105553) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
