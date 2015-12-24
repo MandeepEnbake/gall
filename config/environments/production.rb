1caddc039c1ff949fc6e451d494af21f00cdbd4c
Mandeep Rawat <mandeep.enbake@gmail.com>
2015-12-24 18:00:32 +0530

    some commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
     :port => 587,
     :domain => ENV["gamil.com"],
     :user_name => ENV["mandeep.enbake@gmail.com"],
-    :password => ENV["enbake123"],
+    :password => ENV["somepasword"],
     :authentication => :plain,
     :enable_starttls_auto => true }
 
