157755b5f23cfc20505487f12adfd89bac2b86a7
Mandeep Rawat <mandeep.enbake@gmail.com>
2015-12-24 17:42:11 +0530

    hmmmmmm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,9 +12,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => ENV[“GMAIL_DOMAIN”],
-    :user_name => ENV[“GMAIL_USERNAME”],
-    :password => ENV[“GMAIL_PASSWORD”],
+    :domain => ENV["gamil.com"],
+    :user_name => ENV["mandeep.enbake@gmail.com"],
+    :password => ENV["enbake123"],
     :authentication => :plain,
     :enable_starttls_auto => true }
 
